225eb0934c5451dbda18a4a4e6c79d14415355de
한인철 <gsw05018@naver.com>
2024-02-16 00:28:14 +0900

    자ë°ìJump to java : javaCood architecture

---
+++ src/Java.java
@@ -0,0 +1,14 @@
+public class Java {
+	public static void main(String[] args){
+	}
+	private String message;
+
+	public Java(String message){
+		this.message = message;
+	}
+}
+// private String message : 속성값을 나타냄
+// public Java(String message){ this.message = message; } : 생성자를 나타냄
+// 생성자 : 클래스 인스턴스를 생성할 때 실행되는 메서드로, 주로 속성을 초기화하는 용도로 사용
+// static : 메서드에 static 키워드가 붙으면 클래스 메서드가 되어 객체를 만들지 않아도 클래스명.메서드명 형태로 호출이 가능하다
+
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main ( String[] args ) {
+		System.out.println ( "Hello world!" );
+	}
+}
+// public : 메서드의 접근 제어자로 누구나 이 메서드에 접근 가능
+// static : 메서드에 static이 지정되어 있으면 이 메서드는 인스턴스를 생성하지 않아도 실행할 수 있다는 것을 의미
+// void : 메서드의 리턴값이 없음을 의미
+// String[] : 문자열을 나타내는 자바의 자료형 []은 배열을 의미
+// args : 자료형의 변수명
+// System.out.println : 표준 출력으로 데이터를 보내는 자바의 내장 메서드로 문자열을 화면에 출력
