097f0f2102a0ec1bde8354b5538659b363a5715d
한인철 <gsw05018@naver.com>
2024-02-23 19:33:47 +0900

    jump to java : array

---
+++ src/arrayDev.java
@@ -0,0 +1,31 @@
+public class arrayDev {
+	public static void main(String[] args ){
+		int[] odds  = {1, 2, 3, 4, 5, 6};
+
+		System.out.println ( odds[1] ); // 2
+
+		String[] weeks = {"월", " 화", "수", "목"};
+		String[] weeKs = new String[7]; // [] 숫자를 넣지 않으면 컴파일 에러가 나옴
+		weeKs[0] = "월"; 
+		weeKs[1] = "화";
+		weeKs[2] = "수";
+		weeKs[3] = "목";
+		weeKs[4] = "금";
+		weeKs[5] = "토";
+		weeKs[6] = "일";
+
+		System.out.println ( weeKs.length );
+		// 7 length > 길이
+		
+		System.out.println ( weeKs[3] );
+		// 목
+
+		// 배열의 길이 구하기
+		for(int i = 0; i < weeKs.length; i++){
+			System.out.println ( weeKs[i] );
+		}
+		// 반복문으로 배열을 반복
+	}
+
+
+}
